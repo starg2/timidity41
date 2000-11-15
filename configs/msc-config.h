7422cf01d09dfdc34d19088487b17923afa70d97
Masanao Izumo <mo@goice.co.jp>
2000-11-15 12:59:05 +0900

    TiMidity++-2.10.3a2

--- configs/msc-config.h
+++ configs/msc-config.h
@@ -261,7 +261,7 @@
 
 /* In VDS Macro AAA=BBB is not available. */
 #define __W32__
-#define TIMID_VERSION	"2.10.3a1"
+#define TIMID_VERSION	"2.10.3a2"
 #define DEFAULT_PATH	".\\"
 #define AU_W32
 #define WINSOCK
