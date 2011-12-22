6ca65d8b3dc3879a1b9dc72d64f0c0ed6b3aae42
Shoichi Tamuki <tamuki@linet.gr.jp>
2011-12-22 12:47:55 +0000

    Fix to fit with libpng 1.5 and newer

--- interface/x_sherry.c
+++ interface/x_sherry.c
@@ -781,8 +781,7 @@ static void sry_pal_v2r(uint8 *data)
 
 static void png_read_func(png_structp png_ptr, char *buff, size_t n)
 {
-    struct timidity_file *tf;
-    tf = (struct timidity_file *)png_ptr->io_ptr;
+    struct timidity_file *tf = png_get_io_ptr(png_ptr);
     tf_read(buff, 1, n, tf);
 }
 
@@ -909,7 +908,11 @@ static void sry_load_png(uint8 *data)
 	{
 	    if(png_get_valid(pngPtr, infoPtr, PNG_INFO_hIST))
 		png_get_hIST(pngPtr, infoPtr, &hist);
+#if PNG_LIBPNG_VER >= 10402
+	    png_set_quantize(pngPtr, palette,
+#else
 	    png_set_dither(pngPtr, palette,
+#endif
 			   numPalette, MAX_SCREEN_COLORS, hist, 1);
 	}
     }
@@ -934,13 +937,14 @@ static void sry_load_png(uint8 *data)
 		}
 	    }
 	}
+#if PNG_LIBPNG_VER >= 10402
+	png_set_quantize(pngPtr, stdColorCube,
+#else
 	png_set_dither(pngPtr, stdColorCube,
+#endif
 		       6*7*6, MAX_SCREEN_COLORS,
 		       NULL, 1);
-	/*???*/
-	png_set_PLTE(pngPtr, infoPtr, pngPtr->palette, pngPtr->num_palette);
-	palette = pngPtr->palette;
-	numPalette = pngPtr->num_palette;
+	png_get_PLTE(pngPtr, infoPtr, &palette, &numPalette);
     }
 
     if(png_get_valid(pngPtr, infoPtr, PNG_INFO_tRNS))
