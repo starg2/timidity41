d587bb04f13399f3a2c7716f5dbdfcef936a73a9
Yair K <cesium2@gmail.com>
2012-05-18 10:38:14 +0200

    Improve timidity default settings
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- timidity/timidity.h
+++ timidity/timidity.h
@@ -61,6 +61,7 @@
 
 #define DECOMPRESSOR_LIST { \
 			      ".gz",  "gunzip -c %s", \
+			      ".xz",  "xzcat %s", \
 			      ".bz2", "bunzip2 -c %s", \
 			      ".Z",   "zcat %s", \
 			      ".zip", "unzip -p %s", \
@@ -110,7 +111,7 @@ typedef double FLOAT_T;
    sound terrible as no lowpass filtering is performed on most
    instruments before resampling. */
 #define MIN_OUTPUT_RATE 	4000
-#define MAX_OUTPUT_RATE 	65000
+#define MAX_OUTPUT_RATE 	400000
 
 
 /* Master volume in percent. */
