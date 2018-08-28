98254ed72c449df886aabee5e595f0c1823434d2
TAMUKI Shoichi <tamuki@linet.gr.jp>
2018-08-29 08:33:39 +0900

    Bump version to 2.15.0
    - Update version to 2.15.0.
    - Update copyright year to 2018.
    - Update NEWS.
    
    Signed-off-by: TAMUKI Shoichi <tamuki@linet.gr.jp>

--- interface/w32g_i.c
+++ interface/w32g_i.c
@@ -1,6 +1,6 @@
 /*
     TiMidity++ -- MIDI to WAVE converter and player
-    Copyright (C) 1999-2014 Masanao Izumo <iz@onicos.co.jp>
+    Copyright (C) 1999-2018 Masanao Izumo <iz@onicos.co.jp>
     Copyright (C) 1995 Tuukka Toivonen <tt@cgs.fi>
 
     This program is free software; you can redistribute it and/or modify
@@ -3827,7 +3827,7 @@ static void TiMidityWnd(HWND hParentWnd)
 	char TiMidityText[2024];
   sprintf(TiMidityText,
 " TiMidity++ %s%s -- MIDI to WAVE converter and player" NLS
-" Copyright (C) 1999-2014 Masanao Izumo <iz@onicos.co.jp>" NLS
+" Copyright (C) 1999-2018 Masanao Izumo <iz@onicos.co.jp>" NLS
 " Copyright (C) 1995 Tuukka Toivonen <tt@cgs.fi>" NLS
 NLS
 " Win32 version by Davide Moretti <dmoretti@iper.net>" NLS
